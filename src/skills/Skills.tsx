b126ac2270cc2d01bb34efcb4166acc5978c1ca0
ArtyomKub <kub.killjoy@gmail.com>
2023-02-01 17:57:39 +0300

    Add new three block 'Skill'

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Skills.module.css'
 import stylesContainer from '../common/styles/Container.module.css'
+import {Skill} from "./skill/Skill";
 
 export const Skills = () => {
     return (
@@ -8,7 +9,12 @@ export const Skills = () => {
             <div className={`${stylesContainer.container} ${style.skillsContainer}`}>
                 <h2 className={style.title}>MY EXPERIENCES</h2>
                 <div className={style.skills}>
-
+                    <Skill title={'CSS'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}/>
+                    <Skill title={'JavaScript'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}/>
+                    <Skill title={'React'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}/>
                 </div>
             </div>
         </div>
