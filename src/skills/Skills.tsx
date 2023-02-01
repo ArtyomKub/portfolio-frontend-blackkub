65d5c45e98c151353bb925e4bcffd50209f673c1
ArtyomKub <kub.killjoy@gmail.com>
2023-02-01 16:26:25 +0300

    Add new block 'Skills'

---
+++ src/skills/Skills.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Skills.module.css'
+
+export const Skills = () => {
+    return (
+        <div className={styles.skillsBlock}>
+
+        </div>
+    );
+};
